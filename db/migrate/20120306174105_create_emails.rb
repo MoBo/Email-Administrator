e9f3de37d2fb663bafd7b5b6ef7496349095d71b
Saulius Archipovas <ich@sauli.de>
2012-03-06 18:49:37 +0100

    Finish addModelsAndMigration

---
+++ db/migrate/20120306174105_create_emails.rb
@@ -0,0 +1,16 @@
+class CreateEmails < ActiveRecord::Migration
+  def change
+    create_table :emails do |t|
+      t.string :address
+      t.string :password
+      t.text :comment
+      t.date :expires
+      t.string :path
+      t.string :foward_email
+      t.boolean :receive
+      t.string :alt_email
+
+      t.timestamps
+    end
+  end
+end

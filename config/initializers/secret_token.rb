754b66120cef36ab40651eb62ee72ff75c21284c
Saulius Archipovas <ich@sauli.de>
2012-03-06 18:34:14 +0100

    Add database diagram

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailAdministrator::Application.config.secret_token = 'd6d4984d3474b09cc68498a0b1e403240cf86422744c23fe155274e36e5dee61a60a097fe134d38bcd9f488411a7c01e11f71891e6465823b3808f465ef25b1f'

44a8bdd8107a21c3a87ea6956600859fbc3af342
Saulius Archipovas <ich@sauli.de>
2012-03-14 15:53:17 +0100

    Add Postfix Log Parser to rails project

---
+++ db/migrate/20120314142245_server_log.rb
@@ -0,0 +1,42 @@
+class ServerLog < ActiveRecord::Migration
+  def change
+    create_table :incoming_logs do |t|
+        t.string :server
+        t.string :queue_id 
+        t.string :message_id 
+        t.datetime :arrive_time
+        t.string :hostname 
+        t.string :ipaddr
+        t.integer :uid
+        t.string :user
+        t.string :sender
+        t.string :sdomain
+        t.integer :size 
+        t.boolean :old
+    end  
+    
+    add_index :incoming_logs, :queue_id
+    add_index :incoming_logs, :sender
+    add_index :incoming_logs, :sdomain
+    
+    create_table :outgoing_logs do |t|
+        t.datetime :action_time
+        t.string :recipient
+        t.string :rdomain 
+        t.string :status 
+        t.string :relay 
+        t.integer :int
+        t.text :text
+    end
+    
+    add_index :outgoing_logs, :recipient
+    add_index :outgoing_logs, :rdomain
+    add_index :outgoing_logs, :action_time
+    
+    create_table :work_logs do |t|
+        t.string :filename
+        t.integer :inode
+        t.integer :int
+    end  
+  end
+end
---
+++ lib/tasks/parse_logs.rake
@@ -0,0 +1,6 @@
+namespace :log do
+  desc "Fill database with sample data"
+  task parse: :environment do
+    
+  end
+end

7f84d82665bffa5d4ece27db752c990034b7b6f4
Saulius Archipovas <ich@sauli.de>
2012-03-15 15:07:19 +0100

    adding simple view

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -14,4 +14,12 @@ module EmailHelper
   def forwards_badge(email)
     image_tag("email-forwards.png")+content_tag(:span, email.forwards.count, :class => "label label-info") unless email.forwards.empty?
   end
+  
+  def can_receive(email)
+    content_tag(:span, "R", :class => "label label-info") unless email.can_receive?
+  end
+  
+  def can_send(email)
+    content_tag(:span, "S", :class => "label label-info") unless email.can_send?
+  end
 end

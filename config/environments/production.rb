f47980523c8f1eec1bb59c03b93e87738a1e7c61
MoBo <moritz.bode@gmail.com>
2012-03-16 10:18:56 +0100

    Fix default url for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,5 @@ EmailAdministrator::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   
   
-  config.action_mailer.default_url_options = { :host => '134.102.26.70:8080/email-admin/' }
+  config.action_mailer.default_url_options = { :host => '134.102.26.70:8080/email-admin' }
 end
